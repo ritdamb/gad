16b5ab8d45aba9407e99d568ec8c7453ba8a746b
Luigi Amitrano <Luigi Amitrano>
2015-11-14 11:01:49 +0100

    cambiate le costanti in uppercase

--- app/src/main/java/it/unisa/gad/seriestracker/Constant/URLConstant.java
+++ app/src/main/java/it/unisa/gad/seriestracker/Constant/URLConstant.java
@@ -4,6 +4,6 @@ package it.unisa.gad.seriestracker.Constant;
  * Created by ludimar on 09/11/15.
  */
 public class URLConstant {
-    public static final String tvComGetNewsUrl = "http://www.tv.com/shows/";
-    public static final String tvComGetLinkShowUrl = "http://www.tv.com/shows/";
+    public static final String TVCOM_GETNEWS_URL= "http://www.tv.com/shows/";
+    public static final String TVCOM_GETLINK_SHOWURL= "http://www.tv.com/shows/";
 }
