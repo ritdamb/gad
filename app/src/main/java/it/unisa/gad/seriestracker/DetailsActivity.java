ad91b4d43a4af5c56985929f97a2b4cb57e01b67
ludimar <dimartinoluigi@live.it>
2015-12-16 17:23:28 +0100

    Fixata i Dettagli di una Serie.

--- app/src/main/java/it/unisa/gad/seriestracker/DetailsActivity.java
+++ app/src/main/java/it/unisa/gad/seriestracker/DetailsActivity.java
@@ -7,19 +7,39 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.androidquery.AQuery;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import it.unisa.gad.seriestracker.Domain.Series;
 import it.unisa.gad.seriestracker.util.ApplicationVariables;
+import it.unisa.gad.seriestracker.util.HtmlPageParser;
 
 public class DetailsActivity extends Activity {
 
@@ -27,17 +47,31 @@ public class DetailsActivity extends Activity {
 	private File file ;
 	private ArrayList<String> arrayTelefilm;
 
+	private Document doc;
+	private TextView tvDescription;
+	private TextView tvTitle;
+	private TextView tvGenre;
+	private ImageView seriesBanner;
+	private Bundle arg;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.details_activity);
 
-		TextView tvTitle = (TextView) findViewById(R.id.titleTelefilm);
-		TextView tvDescription = (TextView) findViewById(R.id.textDetails);
+		tvTitle = (TextView) findViewById(R.id.titleTelefilm);
+		tvDescription = (TextView) findViewById(R.id.textDetails);
+		tvGenre = (TextView) findViewById(R.id.telefilmGenre);
+		seriesBanner = (ImageView) findViewById(R.id.seriesDetBanner);
 
-		Bundle arg = getIntent().getExtras();
+		arg = getIntent().getExtras();
 		nameTelefilm = arg.getString(Series.NAME_TELEFILM);
+		Bitmap b = BitmapFactory.decodeByteArray(arg.getByteArray("img"),0,arg.getByteArray("img").length);
+
+		seriesBanner.setImageBitmap(b);
 		tvTitle.setText(nameTelefilm);
+
 
 		Button follow = (Button) findViewById(R.id.buttonFollow);
 		follow.setOnClickListener(new MyButtonClickListener());
@@ -56,8 +90,20 @@ public class DetailsActivity extends Activity {
 
 
 		}
+	}
 
+	protected void onStart() {
+		super.onStart();
 
+		String seriesNameMod = nameTelefilm.replace(" ","_");
+		String urlMod = "https://en.wikipedia.org/wiki/"+seriesNameMod+"_(TV_series)";
+		try {
+			URL url = new URL(urlMod);
+			BackgroundTask backgroundTask = new BackgroundTask("//div[@id='bodyContent']",url,this);
+			backgroundTask.execute();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public class MyButtonClickListener implements View.OnClickListener {
@@ -93,6 +139,89 @@ public class DetailsActivity extends Activity {
 			}
 
 	}
-	
+
+
+	private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+
+		private URL url;
+		private String xPath;
+		private HtmlPageParser p;
+		private XPath xPathObj;
+		private ProgressDialog dialog;
+		private String wikiDescription;
+		private Context context;
+		private String genreText;
+		private Boolean flag;
+
+
+		public BackgroundTask(String xPath, URL url, Context context) {
+			this.xPath = xPath;
+			this.url = url;
+			this.context = context;
+			flag = false;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			p = new HtmlPageParser();
+			p.setUrl(url);
+			p.setXPath(xPath);
+			dialog = new ProgressDialog(context);
+			dialog.setCancelable(true);
+			dialog.setTitle("Loading...");
+			dialog.setMessage("Loading The Selected Series...");
+			dialog.show();
+
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+
+			//INSERIRE I VALORI NEI TEXFIELD
+			if(flag){
+				tvDescription.setText(wikiDescription);
+				tvGenre.setText("GENRE: "+genreText);
+			} else {
+				tvGenre.setText("");
+				tvDescription.setText(arg.getString("description"));
+			}
+			dialog.dismiss();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			p.perform();
+			doc = p.getResultXmlDocument();
+			xPathObj = XPathFactory.newInstance().newXPath();
+			wikiDescription = "";
+			try {
+				Node genre = (Node) xPathObj.compile("//table/tbody/tr/td[@class='category']").evaluate(doc, XPathConstants.NODE);
+				if(genre == null) {
+					flag = false;
+				} else {
+				flag = true;
+				genreText = genre.getTextContent();
+				genreText = genreText.trim();
+				genreText = genreText.replace("\n", " ");
+				for(int i = 0; i < doc.getDocumentElement().getElementsByTagName("p").getLength() ; i++){
+					if(doc.getDocumentElement().getElementsByTagName("p").item(i).getParentNode().getParentNode().getNodeName().equals("div")) {
+						if(doc.getDocumentElement().getElementsByTagName("p").item(i).getTextContent().length() > 30) {
+							if( (wikiDescription+doc.getDocumentElement().getElementsByTagName("p").item(i).getTextContent()).length() > 2000 ) {
+								break;
+							} else {
+								wikiDescription = wikiDescription+"\n"+doc.getDocumentElement().getElementsByTagName("p").item(i).getTextContent();
+							}
+						}
+					}
+				}
+				}
+			} catch (XPathExpressionException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
+
 
 }
