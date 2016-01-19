64fff92239c2705564915dc0b7351ff714e8ad3f
Rita <rita.dambrosio@gmail.com>
2016-01-19 15:38:15 +0100

    aggiunta xpath tv show time

--- app/src/main/java/it/unisa/gad/seriestracker/Constant/XPathConstant.java
+++ app/src/main/java/it/unisa/gad/seriestracker/Constant/XPathConstant.java
@@ -9,7 +9,7 @@ public class XPathConstant {
 //  public static final String TV_GUIDE_TRENDING_TONIGHT ="//*[@id='content']/div[1]/div[1]/ul/li";
     public static final String TV_GUIDE_TRENDING_TONIGHT = "//li[@class='listings-program']";
 
-
+    public static final String TV_SHOW_MANAGER_DAYS_FOR_NEXT_EP="//div[@style=\"margin-top: 20px; font-size:18px;\"]//span[@class=\"odometer-value\"]/text()";
     public static final String TV_COM_GET_NEWS_DETAILS_NEWS="//div[@class =\"actions_bar\"]/following-sibling::div[1]";
     public static final String TV_COM_GET_NEWS_DETAILS_SHOWS="//div[@class =\"content editable complex\"]";
     public static final String TV_COM_NEWS_GET_NEWS="//div[@class=\"m news_list _standard_module _clear_fix\"]/ul[@class =\"stories\"]";
